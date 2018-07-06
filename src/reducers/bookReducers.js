78b5503b58c640577e10d5df49628575c8b93a7e
ZainPathan <zainpathan@gmail.com>
2018-07-06 17:48:31 +0530

    Committing missed changes in bookReducers.js

--- src/reducers/bookReducers.js
+++ src/reducers/bookReducers.js
@@ -2,7 +2,11 @@ export default ( state = [], action) => {
     switch(action.type) {
         //Check if action dispatched is CREATE_BOOK and act on it
         case 'CREATE_BOOK': 
-            state.push(action.book);
+            //state.push(action.book);
+            return [
+                ...state,
+                Object.assign({}, action.book)
+            ];
         default: 
             return state;
     }
